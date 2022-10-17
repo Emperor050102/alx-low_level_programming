7dec608f39a8226442240016a3b319874f956290
Athagbolo Glory Osigwe <gloryatthagbolo@gmail.com>
2022-10-16 20:46:55 -0700

    new puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,3 @@
 #!/bin/bash
-include <stdio.h> 
-int main() { put("Programming is like building a multilingual puzzle\n"); return 0; }
+#include <stdio.h> 
+int main() { puts("Programming is like building a multilingual puzzle\n"); return 0; }
