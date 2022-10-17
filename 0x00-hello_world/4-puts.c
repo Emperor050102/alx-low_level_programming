a163969dead3e2dfc8f95da19e0edd0d25d0f4f4
Athagbolo Glory Osigwe <gloryatthagbolo@gmail.com>
2022-10-17 07:46:57 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,12 @@
 
 /**
  * main - print the string in the put function
- *
  * Description: using the main function
  * this program prints "Programming is like building a multingual puzzle
  * Return: 0
  */
 int main(void) 
 {
-         puts("\"Programming is like building a multilingual puzzle");
-         return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
