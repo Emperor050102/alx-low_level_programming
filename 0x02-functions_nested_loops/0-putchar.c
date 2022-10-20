dafc120a9c27110563c1f78bb5cbd17cd5b679da
Athagbolo Glory Osigwe <gloryatthagbolo@gmail.com>
2022-10-20 13:29:08 -0700

    putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/*we would use the manin to input variables
+ * for would be used to assign the parameters
+ * and we would use _putcher to print to screen
+ * return 0
+ */
+
+int main(void) {
+	int i;
+	for(i=o, i<5, i++) {
+		_putcher("%d\n" , i);
+	}
+	return 0;
+}
